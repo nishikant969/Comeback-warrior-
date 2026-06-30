821b3d693ff90e61982aa30130f05ad5d4d668c0
nishikant969 <nishikantk2007@gmail.com>
2026-06-30 16:18:34 +0530

    Create todo.php

---
+++ todo.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>To-Do List</title>
+</head>
+<body>
+    <h2>To-Do List</h2>
+    <form action="todo.php" method="post">
+        <label>New Task:</label>
+        <input type="text" name="task" required>
+        <button type="submit" name="add_task">Add Task</button>
+    </form>
+    
+    <h3>Tasks</h3>
+    
