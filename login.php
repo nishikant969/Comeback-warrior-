c81f0cedf6e37144402e6052ba2eb1402fdcf60b
nishikant969 <nishikantk2007@gmail.com>
2026-06-30 16:08:50 +0530

    Create login.php

---
+++ login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <h2>Login</h2>
+    <form action="login.php" method="post">
+        <label>Username:</label>
+        <input type="text" name="username" required><br><br>
+        <label>Password:</label>
+        <input type="password" name="password" required><br><br>
+        <button type="submit" name="submit">Login</button>
+    </form>
+</body>
+</html>
