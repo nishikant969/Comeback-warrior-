60f904f888f482c491abc5bc730e26eeb645500a
nishikant969 <nishikantk2007@gmail.com>
2026-06-30 16:15:27 +0530

    Create tracker.php

---
+++ tracker.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Daily Goal Tracker</title>
+</head>
+<body>
+    <h2>Daily Goal Tracker</h2>
+    <form action="tracker.php" method="post">
+        <label>New Goal:</label>
+        <input type="text" name="goal" required>
+        <button type="submit" name="add_goal">Add Goal</button>
+    </form>
+    
+    <h3>Your Goals</h3>
+
